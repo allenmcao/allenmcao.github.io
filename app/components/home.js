05552527ba6e720017f75409a502638a05b22b30
Allen Cao <allen.m.cao@gmail.com>
2017-01-28 02:40:39 -0800

    trying to transition with transition group and tweenmax

--- app/components/home.js
+++ app/components/home.js
@@ -2,52 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group' 
+import ReactTransitionGroup from 'react-addons-transition-group'
 import { styles } from './styles/home.scss'
+import { TweenMax } from 'gsap'
 
 import { Link } from 'react-router'
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {mounted: false};
+        // this.state = {mounted: false};
     }
 
-    getInitialState() {
-        return { mounted: false };
-    }
+    // getInitialState() {
+    //     return { mounted: false };
+    // }
 
-    componentDidMount() {
-        this.setState({ mounted: true });
-        // var elem = this.getDOMNode();
-        // // Set the opacity of the element to 0
-        // elem.style.opacity = 0;
-        // window.requestAnimationFrame(function() {
-        //     // Now set a transition on the opacity
-        //     elem.style.transition = "width 2500ms ease-in";
-        //     // and set the opacity to 1
-        // });
-        var myEl = ReactDOM.findDOMNode(this.refs.namee);
-        // myEl.className = "name-load";
+    // componentDidMount() {
+    //     this.setState({ mounted: true });
+    //     // var elem = this.getDOMNode();
+    //     // // Set the opacity of the element to 0
+    //     // elem.style.opacity = 0;
+    //     // window.requestAnimationFrame(function() {
+    //     //     // Now set a transition on the opacity
+    //     //     elem.style.transition = "width 2500ms ease-in";
+    //     //     // and set the opacity to 1
+    //     // });
+    //     var myEl = ReactDOM.findDOMNode(this.refs.namee);
+    //     // myEl.className = "name-load";
+    // }
+
+    componentWillAppear() {
+        console.log("willEnter");
+    }
+    componentWillLeave() {
+        var myEl = ReactDom.findDOMNode(this.refs.namee)
+        console.log("FUCK");
+        TweenMax.fromTo(".name", 6, {color: black}, {color: white})
     }
     render () {
-    let horizs = null;
-    let blah = null
-    if (this.state.mounted) {
-        horizs = (
-        <div className='hrDiv' key="base">
-            <hr className='hr1' key="one"/>
-            <hr className='hr2' key="two"/>
-            <hr className='hr3' key="three"/>
-        </div>);
-    }
-    return (
-        <div className='home'>
-            
-            <h2 ref="namee" className='name'>ALLEN CAO</h2>
-            {horizs}
-            <p>University of California, Berkeley</p>
-        </div>
-    );
+        return (
+            <div className='home'>
+                
+                <h2 ref="namee" className='name'>ALLEN CAO</h2>
+                <div className='hrDiv' key="base">
+                    <hr className='hr1' key="one"/>
+                    <hr className='hr2' key="two"/>
+                    <hr className='hr3' key="three"/>
+                </div>
+                <p>University of California, Berkeley</p>
+            </div>
+        );
     }
 }
 // <Link to='/aboutme' className='navWrapper'>PODSFJPSDOF</Link>
--- app/components/main.js
+++ app/components/main.js
@@ -4,16 +4,22 @@ import Navigation from './navigation'
 import AboutMe from './aboutme'
 import Home from './home'
 
+import ReactTransitionGroup from 'react-addons-transition-group'
 import { styles } from './styles/main.scss'
 
 export default class Main extends React.Component {
   render () {
+    const { location } = this.props;
     return (
         <div className='main'>
-            <Navigation />
-            <div className='content'>
-                {this.props.children}
-            </div>
+            <ReactTransitionGroup>
+                <Navigation key="n"/>
+                <div className='content' key="c">
+                    {React.cloneElement(this.props.children, {
+                      key: location.pathname,
+                    })}
+                </div>
+            </ReactTransitionGroup>
         </div>        
         );
   }
