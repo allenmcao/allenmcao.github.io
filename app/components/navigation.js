caaacf2b7db9670e3f3114dc0dda7ade76242aae
Allen Cao <allen.m.cao@gmail.com>
2018-09-03 04:22:31 -0700

    *Made covering bar reach all the way, adjusted nav margins

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -5,51 +5,34 @@ import { Link } from 'react-router';
 import { styles } from './styles/navigation.scss'
 
 export default class Navigation extends React.Component {
-  render () {
-    return (
-        <nav>
-            <GithubCorner 
-                href='https://github.com/allenmcao/allenmcao.github.io'
-                direction='left'
-                bannerColor='black'
-                />
-            <div className='nameWrapper'>
-                <div className='name1'>ALLEN</div>
-                <div className='name2'>CAO</div>
-            </div>
-            <div className='navWrapper'>
-                <ul>
-                    <li>
-                        <Link className='linky' to='home'>Home</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='aboutme'>About</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='work'>Work</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='education'>Education</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='skills'>Skills</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='projects'>Projects</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                    <li>
-                        <Link className='linky' to='references'>References</Link>
-                        <span>&nbsp;</span>
-                    </li>
-                </ul>
-            </div>
-        </nav>       
-    );
-  }
+    navList () {
+        const navs = ['home', 'aboutme', 'work', 'education', 'skills', 'projects', 'references']
+        return navs.map(nav => 
+                            <a href={'#/' + nav}>
+                                <li>
+                                    <span className='linky'>{nav}</span>
+                                    <span className='cover'>&nbsp;</span>
+                                </li>
+                            </a>)
+    }
+    render () {
+        return (
+            <nav>
+                <GithubCorner 
+                    href='https://github.com/allenmcao/allenmcao.github.io'
+                    direction='left'
+                    bannerColor='black'
+                    />
+                <div className='nameWrapper'>
+                    <div className='name1'>ALLEN</div>
+                    <div className='name2'>CAO</div>
+                </div>
+                <div className='navWrapper'>
+                    <ul>
+                        {this.navList()}
+                    </ul>
+                </div>
+            </nav>       
+        );
+    }
 }
