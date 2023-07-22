c698acf3d25d799b8290dc3d3e27918dd7952568
Allen Cao <allen.m.cao@gmail.com>
2023-07-22 12:08:01 -0700

    working arrow extend animation

--- src/components/SplashAnimation/index.js
+++ src/components/SplashAnimation/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {motion} from 'framer-motion';
+import {motion, useAnimation} from 'framer-motion';
 
 import Expire from 'components/Expire'
 import {BlackCircle, WhiteCircle, WhiteSplatter, BlackSplash} from './Splash'
@@ -7,22 +7,21 @@ import {BlackCircle, WhiteCircle, WhiteSplatter, BlackSplash} from './Splash'
 import { getCurrentDimension, randomize } from 'utils/util';
 
 const arrowLine = {
-    initial: {width: 1},
-    animate: {width: 100},
-    hover: {width: 100, transition: {duration: 0.5, delay: 0}},
+    initial: {width: 70},
+    animate: {width: 70},
+    hover: {width: 100},
 }
 
 const arrowDiv = {
-    initial: {marginLeft: 100, fontSize:'80px', opacity: 0},
+    initial: {marginLeft: -30, marginTop: -60, opacity: 0},
     animate: {
         opacity: [0,1,1],
-        fontSize: ['80px','100px','100px'], 
-        marginLeft: 210, 
-        marginTop: 30, 
+        marginLeft: 20, 
+        marginTop: -55,
         transition: { 
+            duration: 5, 
             delay: 2.25,
-            duration: 6, 
-            times: [0,0.001,1]
+            times: [0,0.1,1]
         }
     },
 }
@@ -59,17 +58,17 @@ const SplashAnimation = () => {
             }
             w += 280;
         }
-        console.log(splas);
         setSplashes(splas);
     }, [])
 
-    const [arrowHover, setArrowHover] = useState(false)
-    function handleMouseEnterArrow() {
-        setArrowHover(true);
-    }
-    function handleMouseLeaveArrow() {
-        setArrowHover(false);
-    }
+    const controls = useAnimation();
+    function handleMouseEnterControls() {
+        controls.start("hover");
+      }
+    
+      function handleMouseLeaveControls() {
+        controls.start("initial");
+      }
 
     return (
         <motion.div  className="absolute w-full h-full overflow-hidden " initial="initial" animate="animate" variants={parentVariant}>
@@ -85,30 +84,31 @@ const SplashAnimation = () => {
                     <WhiteSplatter delay={2.25} size={400} />
                 </motion.div>
 
-                <motion.div className="absolute w-full h-full flex items-center justify-center  z-20">
+                <motion.div className="absolute w-full h-full rotate-12  z-20">
                     <motion.div 
-                            className="absolute font-[Libelsuit] font-bold rotate-[330deg]"
-                            initial={{opacity: 0, fontSize:'80px', marginLeft: -50, marginTop:-30}}
-                            animate={{opacity: [0,1,1],  fontSize: ['80px','100px','100px'], marginLeft: -200, marginTop: -50, transition:{ duration: 5, delay: 2.25, times: [0,0.1,1]}}}
+                            className="absolute inset-x-1/2 inset-y-1/2 font-[Libelsuit] font-bold rotate-[330deg]"
+                            initial={{opacity: 0, fontSize:'90px', marginLeft: -80, marginTop:-60}}
+                            animate={{opacity: [0,1,1],  fontSize: ['90px','100px','100px'], marginLeft: -160, marginTop: -100, transition:{ duration: 4, delay: 2.25, times: [0,0.1,1]}}}
                         >
                             GO
                     </motion.div>
-                    <motion.button 
-                        className="font-[UniSansThin] font-bold rotate-12 "
+                    <motion.button
+                        className="text-[100px] align-middle relative font-[UniSansThin]  font-bold inline inset-x-1/2 inset-y-1/2"
                         initial="initial"
                         animate="animate"
-                        whileHover="hover"
                         variants={arrowDiv}
-                        onMouseEnter={handleMouseEnterArrow}
-                        onMouseLeave={handleMouseLeaveArrow}
+                        onMouseEnter={handleMouseEnterControls}
+                        onMouseLeave={handleMouseLeaveControls}
                     >
                         <motion.span
-                            className="relative top-[-9px] right-[-15px] align-middle rounded-full bg-black w-0 h-4 inline-block"
-                            whileHover="hover"
+                            className="top-[-9px] right-[-70px] relative align-middle rounded-full bg-black h-[18px] w-[70px] inline-block"
+                            animate={controls}
                             variants={arrowLine}
                         >
                         </motion.span>
+                        <span className="relative vertical-align">
                         ➜
+                        </span>
                     </motion.button>
                 </motion.div>
         </motion.div>
